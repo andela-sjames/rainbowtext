bda8e7550a47613df10201daf9713509b33afaa6
andela-sjames <samuel.james@andela.com>
2019-06-22 23:10:07 +0100

    Use shell script to login.

--- docker_login.py
+++
@@ -1,8 +0,0 @@
-import subprocess
-
-
-# configure aws 
-# use shell in the meantime, might include a script later.
-
-# retrieve the login command to use to authenticate your Docker client to your registry.
-subprocess.Popen(["aws", "ecr", "get-login", "--no-include-email", "--region", "us-east-1"])
---
+++ ecr_login.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+# reference: https://docs.aws.amazon.com/AmazonECR/latest/userguide/ECR_GetStarted.html
+# video: https://aws.amazon.com/ecr/getting-started/
+
+# login to ECR
+echo 'logging into ECR'
+aws configure set default.region us-east-1
+aws configure set default.output json
+
+token=$(aws ecr get-authorization-token)
+registry=$(echo "$token" | jq -r .authorizationData[].proxyEndpoint | sed -e 's|https://||g')
+pass=$(echo "$token" | jq -r .authorizationData[].authorizationToken | base64 -D | cut -d: -f2)
+echo "$pass" | docker login -u AWS --password-stdin "$registry" 
+
+echo 'aws auth successful'
