f05e53b1af0228637d8ab8578f778248ce5dcfd4
andela-sjames <samuel.james@andela.com>
2019-06-27 23:43:58 +0100

    Fix app.py

--- app.py
+++ app.py
@@ -8,6 +8,7 @@ app = Flask(__name__)
 def index():
     if request.method == 'GET':
         return render_template("home.html")
-    
+
+
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=8080)
