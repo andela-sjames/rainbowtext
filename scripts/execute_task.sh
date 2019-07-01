180fb4cdb5ccece8ff041659455c7b5c527c6685
andela-sjames <samuel.james@andela.com>
2019-07-01 23:40:02 +0100

    Attach the task execution role policy

--- scripts/execute_task.sh
+++ scripts/execute_task.sh
@@ -1,3 +1,7 @@
 #!/usr/bin/env bash
 
+# create the task execution role:
 aws iam --region us-west-2 create-role --role-name ecsTaskExecutionRole --assume-role-policy-document file://task-execution-assume-role.json
+
+# Attach the task execution role policy:
+aws iam --region us-west-2 attach-role-policy --role-name ecsTaskExecutionRole --policy-arn arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy
