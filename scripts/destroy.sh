d957ac3b562d17e3add01017d3ee2edce0011458
andela-sjames <samuel.james@andela.com>
2019-07-05 15:14:16 +0100

    Use localhost for uwsgi.ini

--- scripts/destroy.sh
+++ scripts/destroy.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 
 # cleanup ecsRainbowtextTaskExecutionRole
-aws iam detach-role-policy --role-name ecsRainbowtextTaskExecutionRole --policy-arn arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy --profile rainbowtext
 aws iam delete-role --role-name ecsRainbowtextTaskExecutionRole --profile rainbowtext
+aws iam detach-role-policy --role-name ecsRainbowtextTaskExecutionRole --policy-arn arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy --profile rainbowtext
 
 # cleanup aws log group
 aws logs delete-log-group --log-group-name rainbowtext
