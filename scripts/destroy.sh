b39a7f898396d8c6e49d98dd0b481f1ded0f3c86
andela-sjames <samuel.james@andela.com>
2019-07-06 02:59:26 +0100

    Re-order.

--- scripts/destroy.sh
+++ scripts/destroy.sh
@@ -1,9 +1,5 @@
 #!/usr/bin/env bash
 
-# cleanup ecsRainbowtextTaskExecutionRole
-aws iam delete-role --role-name ecsRainbowtextTaskExecutionRole --profile rainbowtext
-aws iam detach-role-policy --role-name ecsRainbowtextTaskExecutionRole --policy-arn arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy --profile rainbowtext
-
 # cleanup aws log group
 aws logs delete-log-group --log-group-name rainbowtext
 
@@ -13,3 +9,7 @@ python scripts/del_ecr.py
 # clean up ecs cluster
 ecs-cli compose --file docker-compose.ecs.yml --ecs-params ecs-params.ecs.yml --project-name rainbowtext service down --cluster-config rainbowtext
 ecs-cli down --force --cluster-config rainbowtext
+
+# cleanup ecsRainbowtextTaskExecutionRole
+aws iam detach-role-policy --role-name ecsRainbowtextTaskExecutionRole --policy-arn arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy --profile rainbowtext
+aws iam delete-role --role-name ecsRainbowtextTaskExecutionRole --profile rainbowtext
