c599d8f16599f854ad6d0882e53e184d56c0306f
andela-sjames <samuel.james@andela.com>
2019-08-21 17:46:29 +0000

    Update login to ecr comment

--- scripts/login_ecr.sh
+++ scripts/login_ecr.sh
@@ -6,14 +6,14 @@
 # load env variables (.env is in the same level as shell script.)
 source scripts/.env
 
-# login to ECR
+# setup AWS profile on your shell
 echo 'logging into ECR'
 aws configure set aws_access_key_id "$AWS_ACCESS_KEY_ID" --profile rainbowtext
 aws configure set aws_secret_access_key "$AWS_SECRET_ACCESS_KEY" --profile rainbowtext
 aws configure set default.region us-east-1 --profile rainbowtext
 aws configure set default.output json --profile rainbowtext
 
-# get logging details.
+# get ecr logging details.
 token=$(aws ecr get-authorization-token)
 registry=$(echo "$token" | jq -r .authorizationData[].proxyEndpoint | sed -e 's|https://||g')
 pass=$(echo "$token" | jq -r .authorizationData[].authorizationToken | base64 -D | cut -d: -f2)
